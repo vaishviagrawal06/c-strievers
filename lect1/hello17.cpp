f1e3fbe9d74908534935e37e72e1eecb631fb7ac
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-03-14 00:58:31 +0530

    some inbuilt function to find max. and min.

---
+++ lect1/hello17.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+//some inbuilt functions to find max and min
+int main()
+{
+int num1,num2;
+cin>>num1>>num2;
+// int minimum= min(num1,num2);
+// cout<<minimum;
+
+// int maximum= max(num1,num2);
+// cout<<maximum;
+return 0;
+}
