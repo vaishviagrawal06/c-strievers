7a0c6232d288d32f81cbd0e650e480d91c698457
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-03-14 00:07:17 +0530

    do-while  loop

---
+++ lect1/hello14.cpp
@@ -0,0 +1,22 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+
+// }
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+     int i=2;
+    do{
+ cout<<"vaishvi"<<" "<<i<<endl;
+        i++;
+    }
+    while(i<=1);
+    cout<<i<<endl;
+    return 0;
+    
+}
