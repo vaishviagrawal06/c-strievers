e75fd49ce16bfeab8baa2ef6f8f5c4bf2c882883
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-03-13 21:52:44 +0530

    character

---
+++ lect1/hello4.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// char
+// int main()
+// {
+    
+// char h;
+// cout<<"enter h : ";
+// cin>>h;
+// cout<<h;
+// }
+
+int main()
+{
+    
+char h = 'g';
+cout<<h;
+}
