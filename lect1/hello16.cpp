1b15734ecf82ab310a7ee9690d157f3f2ccaca05
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-03-14 00:37:09 +0530

    sum of 2 no. using function

---
+++ lect1/hello16.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//take two numbers and print its sum
+
+//WAY 1 OF WRITING CODE
+
+// int sum(int a,int b){
+//     int s = a+b;
+//     return s;
+// }
+// int main()
+// {
+//     int a,b;
+//     cin>>a>>b;
+//     int res = sum( a, b);
+//     cout<<res;
+//     return 0;
+
+
+//WAY 2 OF WRITING CODE
+
+int sum(int a,int b){
+    int s = a+b;
+    cout<< s;
+}
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    sum( a, b);
+    return 0;
+
+
+}
+// }
