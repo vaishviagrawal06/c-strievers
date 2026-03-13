b5efbf40151748f6629c8f4725c59f0eeda2567f
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-03-13 23:45:17 +0530

    string in array

--- lect1/hello11.cpp
+++ lect1/hello11.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 int main(){
     string s = "vaishvi";
-cout<<s[3];  // is index par khon sa character store hai 
+    int len = s.size();
+
+    s[len-1]='v';  //changing the value of last index
+cout<<s[len-1];  // last character of string 
+
+// cout<<s[3];  // is index par khon sa character store hai 
 return 0;
 }
