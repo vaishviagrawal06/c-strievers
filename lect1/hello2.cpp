09e4d37d8bb91a94f4f5d4a670cc759907f9cff0
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-03-13 20:59:35 +0530

    input taking

--- lect1/hello2.cpp
+++ lect1/hello2.cpp
@@ -1,11 +1,24 @@
 #include<iostream>
 using namespace std;
 
+// TAKING ONE INPUT 
+
+// int main(){
+//     int x;
+//     cout<<"enter value of x :";
+//     cin>>x;
+//     cout<<"value of x : "<<x;
+//     return 0;
+
+// }
+
+// TAKING TWO INPUT
+
 int main(){
-    int x;
-    cout<<"enter value of x :";
-    cin>>x;
-    cout<<"value of x : "<<x;
+    int x,y;
+    cout<<"enter value of x AND y :";
+    cin>>x>>y;
+    cout<<"value of x : "<< x <<"and y :" <<y;
     return 0;
 
 }
