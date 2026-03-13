7dba17e7796cd31bf1a9d0248bf7e30d1ff2a1bd
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-03-13 22:27:54 +0530

    print weekdays using switch case

---
+++ lect1/hello8.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+ {
+    int day;
+    cin>> day;
+
+    switch( day){
+        case 1:
+        cout << "monday";
+        return 0;
+        case 2:
+        cout << "tuesday";
+        return 0;
+        case 3:
+        cout << "wednesday";
+        return 0;
+        case 4:
+        cout << "thursday";
+        return 0;
+        case 5:
+        cout << "friday";
+        return 0;
+        case 6:
+        cout << "saturday";
+        return 0;
+        case 7:
+        cout << "sunday";
+        return 0;
+        default:
+        cout<< "error generated";
+
+    
+ }
+ return 0;
+ }
