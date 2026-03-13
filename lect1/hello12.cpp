925455a871f0e55b15630debd36e3b29dd5bb923
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-03-13 23:58:08 +0530

    for loop

--- lect1/hello12.cpp
+++ lect1/hello12.cpp
@@ -4,10 +4,12 @@ using namespace std;
 int main()
 {
     //INCREMENT 
+    
     // for (int i =1 ; i <=10;i++){
     //     cout << "vaishvi"<<endl;
     // }
 
+    //DECREMENT
     for (int i =10 ; i >=0;i--){
         cout << "vaishvi"<<endl;
     }
