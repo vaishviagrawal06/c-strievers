39831dc77086bcd78250d35ce179bdce96533ae7
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-03-14 00:18:50 +0530

    functionc

---
+++ lect1/hello15.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//FUNCTIONS
+// are set of code that perform something for you
+//are used to modularise code
+//used to inc readability
+//used to use same code multiple times
+
+
+    void name(){
+        cout<<"vaishvi";
+    }
+int main()
+{
+    name();
+return 0;
+}
