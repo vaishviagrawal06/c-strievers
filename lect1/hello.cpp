5111b0c195a4f31ad97f544efe88cbaefb4a43b1
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-03-13 20:08:25 +0530

    Initial commit - Striver DSA repository

---
+++ lect1/hello.cpp
@@ -0,0 +1,16 @@
+// #include<iostream>
+// #include<stdio.h>
+// using namespace std;
+
+// int main()
+// {
+//     printf("hello world");
+//     return 0;
+// }
+
+#include<iostream>
+int main()
+{
+    std :: cout<<"hey vaishvi!";
+    return 0;
+}
