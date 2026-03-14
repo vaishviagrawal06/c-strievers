55fa045f8e9b89ae2b2ce70bc937e012445c41b5
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-03-14 12:38:20 +0530

    lower left using digits

---
+++ lect3/pattern4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void nNumberTriangle(int n) {
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= i; j++) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    nNumberTriangle(n);
+}
