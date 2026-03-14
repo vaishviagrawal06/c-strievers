2aef7b70b55c80d03a0dd2bcd2106b5203c18e62
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-03-14 14:17:57 +0530

    upper right using *

---
+++ lect3/pattern6.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern(int n)
+{
+    for(int i =1;i<=n;i++){
+        for(int j=n;j>=i;j--){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    int n =4;
+    pattern(n);
+}
