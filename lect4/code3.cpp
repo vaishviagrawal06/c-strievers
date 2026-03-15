2bfe92f9c641c03489f6a2e535005f1862fea7f4
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-03-15 22:09:39 +0530

    pairs in STL in C++

--- lect4/code3.cpp
+++ lect4/code3.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 void explainPair(){
     pair<int,int> p ={1,3};
-    cout<<p.first<< " "<<p.second;
+    cout<<p.first<< " "<<p.second<<" ";
 
-    pair<int ,pair<int,int>> p={1,{3,4}};
+    pair<int ,pair<int,int>> p2={1,{3,4}};
 
-    cout<<p.first<<" "<<p.second.second<<" "<<p.second.first;
+    cout<<p2.first<<" "<<p2.second.second<<" "<<p2.second.first;
 
 }
 
