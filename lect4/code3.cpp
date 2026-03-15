ee4c71dbb5728ef4f986555f9ab06282aebb4a78
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-03-15 21:01:01 +0530

    Pairs

---
+++ lect4/code3.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void explainPair(){
+    pair<int,int> p ={1,3};
+    cout<<p.first<< " "<<p.second;
+
+    pair<int ,pair<int,int>> p={1,{3,4}};
+
+    cout<<p.first<<" "<<p.second.second<<" "<<p.second.first;
+
+}
+
+int main(){
+explainPair();
+}
