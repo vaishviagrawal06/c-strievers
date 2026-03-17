92a86693835a8ed25c3ff2ce8d2e5b8a0fbe44cb
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-03-17 10:51:02 +0530

    basic math REVERSE OF NUMBER

--- lect5/bsicsMath1.cpp
+++ lect5/bsicsMath1.cpp
@@ -1,10 +1,17 @@
  #include<bits/stdc++.h>
  using namespace std;
 
- int count(int n){
-
+ int Count(int n){
+    int count = 0;
+    while(n>0)
+    {
+        int lastDigit = n%10;
+        count ++;
+        n=n/10;
+    }
+    return count;
  }
  int main()
  {
-    
+    Count(n);
  }
