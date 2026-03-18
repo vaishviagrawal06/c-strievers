66095503ebad57a2d1915dc34b34c8cb7a081378
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-03-18 15:39:18 +0530

    reverse a number

--- lect5/basicMath2.cpp
+++ lect5/basicMath2.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main(){
-    
+    int n;
+    cin>>n;
+    int rev = 0;
+    while(n>0){
+        int lastdig = n%10;
+        rev=(rev*10)+lastdig;
+        n=n/10;
+    }
+    cout<<rev;
 }
