b0a7c4d35cb18cdda72ee6ee48325a79506b8286
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-03-18 15:57:27 +0530

    Pallindrome of a number

--- lect5/basicMath3.cpp
+++ lect5/basicMath3.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main(){
-    
+    int n;
+    cin>>n;
+    int rev = 0;
+    int dup = n;
+    while(n>0){
+        int lastdig = n%10;
+        rev=(rev*10)+lastdig;
+        n=n/10;
+    }
+
+    if(dup==rev){
+        cout<<"true";
+    }
+    else cout<<"false";
 }
