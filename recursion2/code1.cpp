b7d60feeb1e69af149402e0bc2fb53f3f3e94d32
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-03-21 11:30:18 +0530

    print name using recursion

---
+++ recursion2/code1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n = 0;
+void print()
+{
+    if(n==5)
+    return ;
+    cout<<"vaishvi"<<endl;
+    n++;
+    print();
+}
+
+int main()
+{
+    print();
+    return 0;
+}
