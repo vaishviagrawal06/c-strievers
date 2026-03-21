84ef696cb817ca7accf041d524936fb4644814dd
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-03-21 10:54:53 +0530

    recursion

--- recursion/hello.cpp
+++ recursion/hello.cpp
@@ -1,15 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
-int count = 0;
-void print(int count)
-{
-    if(count ==3)
-    return;
-    count++;
-    cout<<count<<endl;
- print();
-}
 
+int cnt = 0;
+void print ()
+{
+    if(cnt==3)
+    {
+        return ;
+    }
+    cout<<cnt<<endl;
+    cnt++;
+    print();
+}
 int main()
 {
     print();
